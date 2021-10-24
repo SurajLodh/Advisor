23a458aa3bf6fbf15e86958f876af42e3e58626f
SurajLodh <surajlodh10962@gmail.com>
2021-10-24 18:16:35 +0530

    models added

--- Advisor/urls.py
+++ Advisor/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+#media(image) url
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AdvisorCall.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- AdvisorCall/models.py
+++ AdvisorCall/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, blank=True, null=True)
+
+
+class Base(models.Model):
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        
+
+class Advisor(Base):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='image/', null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class BookCall(Base):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=False, null=False)
+    advisor = models.ForeignKey(Advisor, on_delete=models.CASCADE, blank=True, null=True)
+    booking_date = models.DateTimeField(null=False, blank=False, verbose_name=_("Booking DateTime"))
+    
+    def __str__(self):
+        return f"{self.user}"
